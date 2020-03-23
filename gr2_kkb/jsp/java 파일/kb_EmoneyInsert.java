a9a6e3ff4c14edee9083cec247a724d97e13027a
rlqja914 <60081970+rlqja914@users.noreply.github.com>
2020-03-23 18:20:19 +0900

    ㅁㄴㅇㄹ

---
+++ gr2_kkb/jsp/java 파일/kb_EmoneyInsert.java
@@ -0,0 +1,46 @@
+package ZENTAL;
+
+public class kb_EmoneyInsert {
+
+	private String emo_cate;
+	private String bank;
+	private String account;
+	private int Price;
+	
+	public kb_EmoneyInsert() {}
+
+
+	public String getEmo_cate() {
+		return emo_cate;
+	}
+
+	public void setEmo_cate(String emo_cate) {
+		this.emo_cate = emo_cate;
+	}
+
+	public String getBank() {
+		return bank;
+	}
+
+	public void setBank(String bank) {
+		this.bank = bank;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public int getPrice() {
+		return Price;
+	}
+
+	public void setPrice(int price) {
+		Price = price;
+	};
+	
+	
+}
